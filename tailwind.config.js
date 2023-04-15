5df4ccde4b84327b5bfab7b1a772918573555e9c
rabbitxtech <nguyendonganh510@gmail.com>
2023-04-16 00:06:17 +0700

    optimize img and css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
-function withOpacityValue(variable) {
-	return ({ opacityValue }) => {
-		if (opacityValue === undefined) {
-			return `rgb(var(${variable}))`
-		}
-		return `rgb(var(${variable}) / ${opacityValue})`
-	}
-}
+// Enable Opacity for custom theme
 
-const themeColors = {
-	base: withOpacityValue('--bg-color'),
-	text: withOpacityValue('--text-color')
-}
+// function withOpacityValue(variable) {
+// 	return ({ opacityValue }) => {
+// 		if (opacityValue === undefined) {
+// 			return `rgb(var(${variable}))`
+// 		}
+// 		return `rgb(var(${variable}) / ${opacityValue})`
+// 	}
+// }
+
+// const themeColors = {
+// 	base: withOpacityValue('--bg-color'),
+// 	text: withOpacityValue('--text-color')
+// }
 
 module.exports = {
 	darkMode: 'class',
 	content: [
-		'./app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
+		'./app/**/*.{js,ts,jsx,tsx}',
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}'
 	],
