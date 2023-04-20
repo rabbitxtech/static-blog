d9656de7335d500f8ec54bf633195c0a3f7af75e
rabbitxtech <nguyendonganh510@gmail.com>
2023-04-21 00:44:09 +0700

    Add lazy load and blur image per post

--- components/post/MDXImage.tsx
+++ components/post/MDXImage.tsx
@@ -1,13 +1,32 @@
-import { FC, HTMLProps } from 'react'
-import Image from 'next/image'
+'use client'
+
+import { FC, useRef, useEffect, HTMLProps } from 'react'
+
 const MDXImage: FC<HTMLProps<HTMLImageElement>> = (props) => {
+	const imgRef = useRef<HTMLImageElement>(null)
+	useEffect(() => {
+		const observer = new IntersectionObserver((entries, observer) => {
+			entries.forEach((entry: IntersectionObserverEntry) => {
+				if (entry.isIntersecting) {
+					(entry.target as HTMLImageElement).src = (entry.target as HTMLImageElement).dataset.src!
+					setTimeout(()=>entry.target.classList.remove('blur-sm'), 500)
+					observer.unobserve(entry.target)
+				}
+			})
+		},{ threshold: 0.8 })
+		if (imgRef.current) observer.observe(imgRef.current)
+		return () => {
+			observer.disconnect()
+		}
+	}, [])
+
 	return (
 		// eslint-disable-next-line @next/next/no-img-element
 		<img
-			src={props.src}
+			ref={imgRef}
+			data-src={props.src}
 			alt={props.alt || ''}
-			className="max-w-full m-auto my-3"
-			{...props}
+			className="max-w-full m-auto my-3 blur-sm"
 		/>
 	)
 }
