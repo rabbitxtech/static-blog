1344241ba39dea0edbfd72b55c83824fc0e1a4cb
rabbitxtech <nguyendonganh510@gmail.com>
2023-04-21 00:42:41 +0700

    Fadein element only once

--- components/global/FadeInSection.tsx
+++ components/global/FadeInSection.tsx
@@ -3,23 +3,25 @@
 import React, { HTMLProps } from 'react'
 
 const FadeInSection: React.FC<HTMLProps<HTMLDivElement>> = (props) => {
-	const [isVisible, setVisible] = React.useState(true)
 	const domRef = React.useRef<HTMLDivElement>(null)
 	React.useEffect(() => {
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => setVisible(entry.isIntersecting))
+		const observer = new IntersectionObserver((entries, observer) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					if (domRef.current) {
+						domRef.current?.classList.add('is-visible')
+						observer.unobserve(domRef.current)
+					}
+				}
+			})
 		})
 		if (domRef.current) observer.observe(domRef.current)
 		return () => {
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-			if (domRef.current) return observer.unobserve(domRef.current)
+			observer.disconnect()
 		}
 	}, [])
 	return (
-		<div
-			className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
-			ref={domRef}
-		>
+		<div className={'fade-in-section'} ref={domRef}>
 			{props.children}
 		</div>
 	)
