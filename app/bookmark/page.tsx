76229dd4775788ad6da2797fcfd88da8d8239dab
rabbitxtech <nguyendonganh510@gmail.com>
2023-05-07 07:14:24 +0700

    adjust style when add layout

--- app/bookmark/page.tsx
+++ app/bookmark/page.tsx
@@ -4,7 +4,6 @@ const Page = () => {
 	return (
 		<div className="max-w-6xl flex flex-col mx-auto max-md:px-4 px-8">
 			<div className="m-auto max-w-2xl w-full">
-				<div className="my-10 text-6xl font-semibold">Bookmark</div>
 				<BookmarkPost />
 			</div>
 		</div>
